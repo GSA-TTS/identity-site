62cf120921d4371428f4769b905c4f4d9e47ea29
Jonathan Hooper <jonathan.hooper@gsa.gov>
2020-06-03 17:00:17 -0400

    Remove unused GA properties
    **Why**: So that the login.gov GA events are not reported to a cloud.gov property

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,110 +1,112 @@
-import $ from 'jquery';
+import $ from "jquery";
 
-import 'bootstrap/js/affix';
-import 'bootstrap/js/scrollspy';
-import './analytics.js';
+import "bootstrap/js/affix";
+import "bootstrap/js/scrollspy";
 
-require('identity-style-guide/dist/assets/js/main');
+require("identity-style-guide/dist/assets/js/main");
 
-var accordion = require('aria-accordion');
+var accordion = require("aria-accordion");
 
-$(function() {
-
+$(function () {
   // Mobile main nav toggle
 
-  $('#js-mobile-nav-toggle').click( function () {
-    $(this).find('span').toggle();
-    $(this).find('img').toggleClass('display-none');
-    $('header nav').toggleClass('flex');
+  $("#js-mobile-nav-toggle").click(function () {
+    $(this).find("span").toggle();
+    $(this).find("img").toggleClass("display-none");
+    $("header nav").toggleClass("flex");
   });
 
   // Language picker
 
   function languagePicker(trigger, dropdown) {
-    trigger.on('click keypress', function (event) {
+    trigger.on("click keypress", function (event) {
       event.preventDefault();
       var eventType = event.type;
-      if (eventType == 'click' || (eventType == 'keypress' && event.which == 13)) {
-        $(this).parent().toggleClass('focused');
+      if (eventType == "click" || (eventType == "keypress" && event.which == 13)) {
+        $(this).parent().toggleClass("focused");
         dropdown.toggle();
 
-        $(this).attr('aria-expanded', function (i, attr) {
-            return attr == 'true' ? 'false' : 'true'
+        $(this).attr("aria-expanded", function (i, attr) {
+          return attr == "true" ? "false" : "true";
         });
       }
     });
   }
 
-  languagePicker($('#i18n-desktop-toggle > a'), $('#i18n-desktop-dropdown'));
-  languagePicker($('#i18n-mobile-toggle > a'), $('#i18n-mobile-dropdown'));
+  languagePicker($("#i18n-desktop-toggle > a"), $("#i18n-desktop-dropdown"));
+  languagePicker($("#i18n-mobile-toggle > a"), $("#i18n-mobile-dropdown"));
 
   // Dropdown menu
 
-  $('.dropdown').focusin(function () {
-    $(this).addClass('focused');
-  }).focusout(function(){
-    $(this).removeClass('focused');
-  });
+  $(".dropdown")
+    .focusin(function () {
+      $(this).addClass("focused");
+    })
+    .focusout(function () {
+      $(this).removeClass("focused");
+    });
 
-  $(document).on('click touch', function(event) {
+  $(document).on("click touch", function (event) {
     var $target = $(event.target);
-    var $dropdownSm = $('.dropdown-sm');
-    if (!$target.hasClass('dropdown-text')
-      && !$target.hasClass('dropdown-sm')) {
-      $dropdownSm.removeClass('focused').blur();
+    var $dropdownSm = $(".dropdown-sm");
+    if (!$target.hasClass("dropdown-text") && !$target.hasClass("dropdown-sm")) {
+      $dropdownSm.removeClass("focused").blur();
     } else {
-      $dropdownSm.toggleClass('focused');
+      $dropdownSm.toggleClass("focused");
     }
   });
 
   // Affix
 
-  if ($('#pb-nav--side').length ) {
-    $('#pb-nav--side').affix({
+  if ($("#pb-nav--side").length) {
+    $("#pb-nav--side").affix({
       offset: {
-        top: $('#pb-nav--side').offset().top
-      }
+        top: $("#pb-nav--side").offset().top,
+      },
     });
 
-    $(window).resize(function() {
-      $('#pb-nav--side').affix('checkPosition').width($('#pb-nav--side-cntnr').width());
+    $(window).resize(function () {
+      $("#pb-nav--side").affix("checkPosition").width($("#pb-nav--side-cntnr").width());
     });
 
-    $('#pb-nav--side').on('affixed.bs.affix', function() {
-      $('#pb-nav--side').width($('#pb-nav--side-cntnr').width());
+    $("#pb-nav--side").on("affixed.bs.affix", function () {
+      $("#pb-nav--side").width($("#pb-nav--side-cntnr").width());
     });
-
   }
 
   // Smooth scroll
 
-  $('a[href^="#"]').on('click', function(event) {
-
-    if (this.hash !== '') {
+  $('a[href^="#"]').on("click", function (event) {
+    if (this.hash !== "") {
       event.preventDefault();
 
       var hash = this.hash;
 
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          $(hash)
+            .attr("tabindex", -1)
+            .on("blur focusout", function () {
+              $(this).removeAttr("tabindex");
+            })
+            .focus();
 
-        $(hash).attr('tabindex', -1).on('blur focusout', function() {
-          $(this).removeAttr('tabindex');
-        }).focus();
-
-        if (history.pushState) {
-          history.pushState(null, null, hash);
-        } else {
-          window.location.hash = hash;
+          if (history.pushState) {
+            history.pushState(null, null, hash);
+          } else {
+            window.location.hash = hash;
+          }
         }
-      });
+      );
     }
   });
 
-
   // Required element to turn into an accordion
-  var elm = document.querySelector('.js-accordion');
+  var elm = document.querySelector(".js-accordion");
   if (elm) {
     new accordion.Accordion(elm, {}, { reflectStatic: true });
   }
