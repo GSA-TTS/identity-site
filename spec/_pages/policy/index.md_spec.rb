ac509cc6a0330c5f77166a450c6735bf45798e52
Shade L. Jenifer <shade.jenifer@gsa.gov>
2020-09-04 01:14:52 -0400

    Tweaks
    **Why**: External links weren't opening in new tabs
    **Why**: Spanish translation had some French :o
    **Why**: Policy spec was outdated
    
    **How**: Added blank targets in en/es/fr.yml
    **How**: Replaced French w/Spanish in es.yml
    **How**: Moved old policy spec to _pages/policy/index

---
+++ spec/_pages/policy/index.md_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+RSpec.describe 'index.md' do
+  let(:doc) { Nokogiri::HTML(file_at('/policy')) }
+
+  it 'opens external links in a new window' do
+    expect(doc).to open_external_links_in_new_window
+  end
+
+  it 'escapes html correctly' do
+    expect(doc).to properly_escape_html
+  end
+
+  it 'links to valid headings' do
+    expect(doc).to link_to_valid_headers
+  end
+
+  it 'links to valid internal pages' do
+    expect(doc).to link_to_valid_internal_pages
+  end
+
+  it 'has valid links' do
+    expect(doc).to link_to_valid_urls
+  end
+end
