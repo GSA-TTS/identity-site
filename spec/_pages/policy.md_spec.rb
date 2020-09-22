b7ad86197fb7b95826e48118e810212e91fe349a
Zach Margolis <zachary.margolis@gsa.gov>
2020-09-22 11:09:11 -0700

    Remove expectation that external links must open in a new window
    **Why**: We had been ignoring this in a bunch of places anyways

--- spec/_pages/policy.md_spec.rb
+++ spec/_pages/policy.md_spec.rb
@@ -3,10 +3,6 @@ require 'spec_helper'
 RSpec.describe 'policy.md' do
   let(:doc) { Nokogiri::HTML(file_at('/policy')) }
 
-  xit 'opens external links in a new window' do
-    expect(doc).to open_external_links_in_new_window
-  end
-
   it 'escapes html correctly' do
     expect(doc).to properly_escape_html
   end
--- spec/_pages/policy/index.md_spec.rb
+++ spec/_pages/policy/index.md_spec.rb
@@ -3,10 +3,6 @@ require 'spec_helper'
 RSpec.describe '/policy/index.md' do
   let(:doc) { Nokogiri::HTML(file_at('/policy')) }
 
-  it 'opens external links in a new window' do
-    expect(doc).to open_external_links_in_new_window
-  end
-
   it 'escapes html correctly' do
     expect(doc).to properly_escape_html
   end
