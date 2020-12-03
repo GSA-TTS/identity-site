058c77a5d04c075d7dc33d4bc2703ab23c93c5aa
Zach Margolis <zachary.margolis@gsa.gov>
2020-12-03 09:25:04 -0800

    Run RSpec assertions against all HTML pages
    - Fix missing help page titles
    - Fix anchors with trailing spaces
    - Remove now-redundant individual page specs

--- spec/_pages/index.md_spec.rb
+++ spec/_pages/index.md_spec.rb
@@ -3,23 +3,7 @@ require 'spec_helper'
 RSpec.describe 'index.md' do
   let(:doc) { Nokogiri::HTML(file_at('/')) }
 
-  it 'escapes html correctly' do
-    expect(doc).to properly_escape_html
-  end
-
-  xit 'links to valid internal pages' do
-    expect(doc).to link_to_valid_internal_pages
-  end
-
-  it 'has valid links' do
-    expect(doc).to link_to_valid_urls
-  end
-
   xit 'contains specific copy that NewRelic checks for' do
     expect(doc.text).to include('secure access to government services')
-  end
-
-  it 'has a title' do
-    expect(doc).to be_uniquely_titled
   end
 end
