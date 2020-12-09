2da434d4ffbcc667d6d14e988b48d3527358eff2
Andrew Duthie <andrew.duthie@gsa.gov>
2020-12-09 15:18:09 -0500

    Upgrade Webpack, Babel dependencies (#468)
    * Upgrade Webpack, Babel dependencies
    
    **Why**: Preempt potential incompatibilities and future maintenance burden.
    
    * Move Webpack, Babel dependencies to dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,26 @@
-var webpack = require('webpack');
-
 const { NODE_ENV = 'production' } = process.env;
 
-module.exports = {
+module.exports = /** @type {import('webpack').Configuration} */ ({
+  mode: NODE_ENV,
+
   entry: {
     site: './assets/js/main.js',
   },
+
+  target: ['web', 'es5'],
+
   output: {
     filename: 'bundle.js',
     path: __dirname + '/assets/js/build',
   },
 
-  devtool: '#cheap-module-eval-source-map',
-
-  mode: NODE_ENV,
-
   module: {
     rules: [
       {
-        test: /\.(js)$/,
+        test: /\.js$/,
         exclude: /node_modules/,
         use: ['babel-loader'],
       },
     ],
   },
-
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: 'jquery',
-      jQuery: 'jquery',
-    }),
-  ],
-
-  resolve: {
-    extensions: ['.js'],
-  },
-};
+});
