c4937f892424afb1956216841ab5d655c034029f
Zach Margolis <zachary.margolis@gsa.gov>
2021-03-15 09:59:43 -0700

    Use federalist config to manage demo branches

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,13 +2,15 @@ function bindCaptchaValidation() {
   const form = document.getElementById('contact-us-form');
   const error = document.getElementById('captcha-error-message');
 
-  // form.addEventListener('submit', (event) => {
-  //   const captcha = document.getElementById('g-recaptcha-response');
-  //   if (!captcha || !captcha.value) {
-  //     event.preventDefault();
-  //     error.textContent = error.dataset.error;
-  //   }
-  // });
+  if (form.dataset.captchaEnabled === 'true') {
+    form.addEventListener('submit', (event) => {
+      const captcha = document.getElementById('g-recaptcha-response');
+      if (!captcha || !captcha.value) {
+        event.preventDefault();
+        error.textContent = error.dataset.error;
+      }
+    });
+  }
 }
 document.addEventListener('DOMContentLoaded', bindCaptchaValidation);
 
