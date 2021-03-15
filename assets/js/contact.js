fce00c0b2849c9b4689c953cdcd1c7b6a13094d0
Zach Margolis <zachary.margolis@gsa.gov>
2021-03-15 12:26:50 -0700

    Instead of disabling catpcha JS, add debug tag

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -2,15 +2,13 @@ function bindCaptchaValidation() {
   const form = document.getElementById('contact-us-form');
   const error = document.getElementById('captcha-error-message');
 
-  if (form.dataset.captchaEnabled === 'true') {
-    form.addEventListener('submit', (event) => {
-      const captcha = document.getElementById('g-recaptcha-response');
-      if (!captcha || !captcha.value) {
-        event.preventDefault();
-        error.textContent = error.dataset.error;
-      }
-    });
-  }
+  form.addEventListener('submit', (event) => {
+    const captcha = document.getElementById('g-recaptcha-response');
+    if (!captcha || !captcha.value) {
+      event.preventDefault();
+      error.textContent = error.dataset.error;
+    }
+  });
 }
 document.addEventListener('DOMContentLoaded', bindCaptchaValidation);
 
