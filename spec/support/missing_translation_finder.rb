8831ca15a560b939f0485af8b23d538a9ea2f672
Claire Annan <claire.annan@gsa.gov>
2021-04-07 18:02:10 -0400

    Update i18n check to look in _data collection.

--- spec/support/missing_translation_finder.rb
+++ spec/support/missing_translation_finder.rb
@@ -12,7 +12,7 @@ class MissingTranslationFinder
   end
 
   def call
-    translations = find_translations_in_folder('_i18n')
+    translations = find_translations_in_folder('_data')
     translations.reject do |translation|
       translation.spanish_translated? && translation.french_translated?
     end.map(&:key)
@@ -31,7 +31,7 @@ class MissingTranslationFinder
   end
 
   def load_translation_keys_in_folder(folder)
-    yaml = YAML.safe_load(File.read("#{folder}/en.yml"))
+    yaml = YAML.safe_load(File.read("#{folder}/en/settings.yml"))
     load_translation_keys_from_hash(yaml)
   end
 
@@ -49,8 +49,7 @@ class MissingTranslationFinder
   end
 
   def find_translation(key:, locale:)
-    filename_postfix = "#{locale}.yml"
-    filename = "_i18n/#{filename_postfix}"
+    filename = "_data/#{locale}/settings.yml"
     yaml = YAML.safe_load(File.read(filename))
     path = key.split('.')
     yaml.dig(*path)
