d86f802ec7c495ba39c6340a4df334f910d9225d
Jessica Dembe <jessica.dembe@gsa.gov>
2021-05-24 12:18:28 -0400

    fix lint errors

--- assets/js/touchpoints_translations.js
+++ assets/js/touchpoints_translations.js
@@ -1,13 +1,13 @@
-const yesButton=document.querySelector('input[type=submit][value=yes]');
-const noButton=document.querySelector('input[type=submit][value=no]');
+const yesButton = document.querySelector('input[type=submit][value=yes]');
+const noButton = document.querySelector('input[type=submit][value=no]');
 
-if(document.documentElement.lang==='es') {
-    if(yesButton) {
-        yesButton.value="si";
-    }
-} else if(document.documentElement.lang==='fr') {
-    if(yesButton && noButton) {
-        yesButton.value='oui';
-        noButton.value="non";
-    }
+if (document.documentElement.lang === 'es') {
+  if (yesButton) {
+    yesButton.value = 'si';
+  }
+} else if (document.documentElement.lang === 'fr') {
+  if (yesButton && noButton) {
+    yesButton.value = 'oui';
+    noButton.value = 'non';
+  }
 }
