47460b6684bb62349dd89ea8f8b11b171d6b8285
Zach Margolis <zachary.margolis@gsa.gov>
2021-05-24 14:45:57 -0700

    Translate Touchpoints via script.onload callback

--- assets/js/touchpoints_translations.js
+++ assets/js/touchpoints_translations.js
@@ -1,13 +1,20 @@
-const yesButton = document.querySelector('input[type=submit][value=yes]');
-const noButton = document.querySelector('input[type=submit][value=no]');
+/**
+ * Attached to Touchpoints script tag as an onload callback
+ */
+function translateTouchpoints() {
+  const yesButton = document.querySelector('input[type=submit][value=yes]');
+  const noButton = document.querySelector('input[type=submit][value=no]');
 
-if (document.documentElement.lang === 'es') {
-  if (yesButton) {
-    yesButton.value = 'si';
-  }
-} else if (document.documentElement.lang === 'fr') {
   if (yesButton && noButton) {
-    yesButton.value = 'oui';
-    noButton.value = 'non';
+    if (document.documentElement.lang === 'es') {
+      if (yesButton) {
+        yesButton.value = 'si';
+      }
+    } else if (document.documentElement.lang === 'fr') {
+      if (yesButton && noButton) {
+        yesButton.value = 'oui';
+        noButton.value = 'non';
+      }
+    }
   }
 }
