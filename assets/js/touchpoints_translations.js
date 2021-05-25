61b9916321474a4052b5310c1ad88352566f4997
Jessica Dembe <jessica.dembe@gsa.gov>
2021-05-25 11:25:54 -0400

    add translated question to js, html, and yml files

--- assets/js/touchpoints_translations.js
+++ assets/js/touchpoints_translations.js
@@ -1,7 +1,9 @@
 const yesButton = document.querySelector('input[type=submit][value=yes]');
 const noButton = document.querySelector('input[type=submit][value=no]');
+const question = document.querySelectorAll('label')
 const touchpointsRoot = document.getElementById('touchpoints_feedback');
 if (touchpointsRoot) {
   yesButton.value = touchpointsRoot.dataset.yes;
   noButton.value = touchpointsRoot.dataset.no;
+  question[1].outerText = touchpointsRoot.dataset.question;
 }
