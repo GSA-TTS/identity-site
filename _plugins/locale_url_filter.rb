b79b6693b480619ccef27556a8ef69d708dd0e9a
Andrew Duthie <andrew.duthie@gsa.gov>
2021-06-10 10:08:56 -0400

    Rename locale_base_url to locale_url
    **Why**: Since the primary purpose is to generate a URL which is localized, including "base" in the name isn't strictly relevant.

--- _plugins/locale_url_filter.rb
+++ _plugins/locale_url_filter.rb
@@ -1,17 +1,17 @@
 module Jekyll
-  module LocaleBaseUrlFilter
+  module LocaleUrlFilter
     LOCALE_STRING_CONSTANT = '__LOCALE_BASE_URL__'.freeze
 
-    def locale_base_url(path = '')
+    def locale_url(path = '')
       site_base_url = @context.registers[:site].baseurl
       locale_prefix = @context.registers[:page]['base_path']
       site_base_url.to_s + locale_prefix.to_s + path
     end
 
     def replace_locale_base_url(input)
-      input.gsub LOCALE_STRING_CONSTANT, locale_base_url
+      input.gsub LOCALE_STRING_CONSTANT, locale_url
     end
   end
 end
 
-Liquid::Template.register_filter(Jekyll::LocaleBaseUrlFilter)
+Liquid::Template.register_filter(Jekyll::LocaleUrlFilter)
