c0fe8b94358c00d7b0adff2e9849f71c7ffc7630
Andrew Duthie <andrew.duthie@gsa.gov>
2021-07-08 15:56:35 -0400

    Refactor language picker as accordion
    **Why**:
    
    - Semantically, it's essentially the same thing.
    - Avoids needing to maintain our own component implementation. Instead, we can leverage the battle-tested USWDS solution.
    - Because we no longer implement our own solution, we can now eliminate jQuery as a dependency.
    - Better isolation of styles allows for some portability if we want to bring this into the design system.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,4 @@
-import $ from 'jquery';
 import { accordion, accordionCloseButton, banner, navigation } from 'identity-style-guide';
 
 const components = [accordion, accordionCloseButton, banner, navigation];
 components.forEach((component) => component.on());
-
-// Language picker
-
-function languagePicker(trigger, dropdown) {
-  trigger.on('click keypress', function (event) {
-    event.preventDefault();
-    var eventType = event.type;
-    if (eventType == 'click' || (eventType == 'keypress' && event.which == 13)) {
-      dropdown.toggle();
-
-      $(this).attr('aria-expanded', function (i, attr) {
-        return attr == 'true' ? 'false' : 'true';
-      });
-    }
-  });
-}
-
-languagePicker(
-  $('.language-picker__label--button'),
-  $('.language-picker__label--button + .dropdown'),
-);
