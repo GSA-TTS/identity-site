8e178e635e80f4eaa37ea918a430b2abdda5f670
Zach Margolis <zachary.margolis@gsa.gov>
2021-07-14 09:40:06 -0700

    Add custom sort_by_values filter

---
+++ _plugins/sort_by_values.rb
@@ -0,0 +1,13 @@
+module Jekyll
+  module SortByValuesFilter
+    # @param [Hash] hash
+    # @return [Array<Hash>] entry-like hashes with 'key' and 'value'
+    def sort_by_values(hash)
+      hash.
+        sort_by { |_key, value| value }.
+        map { |key, value| { 'key' => key, 'value' => value } }
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::SortByValuesFilter)
