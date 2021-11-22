0ad3ce2751bbc4370249178c56c1db4ceb51d637
Andrew Duthie <andrew.duthie@gsa.gov>
2021-11-22 08:40:51 -0500

    TEMPORARY: Alias identity-style-guide to src

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const { resolve } = require('path');
 const { NODE_ENV = 'production' } = process.env;
 
 module.exports = /** @type {import('webpack').Configuration} */ ({
@@ -8,6 +9,13 @@ module.exports = /** @type {import('webpack').Configuration} */ ({
     contact: './assets/js/contact.js',
     touchpoints_translations: './assets/js/touchpoints_translations.js',
     country_support: './assets/js/country_support.js',
+  },
+
+  resolve: {
+    alias: {
+      // TEMPORARY: Alias `identity-style-guide` to source only while pointing at GitHub branch
+      'identity-style-guide': resolve(__dirname, 'node_modules/identity-style-guide/src/js'),
+    },
   },
 
   target: ['web', 'es5'],
