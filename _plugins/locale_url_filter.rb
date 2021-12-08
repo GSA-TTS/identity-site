bc77e8d8628406ab0862bdfbfe7992f432f570bc
Malick Diarra <malick.diarra@gsa.gov>
2021-12-08 10:19:55 -0500

    Release netlify cms fix rspec

--- _plugins/locale_url_filter.rb
+++ _plugins/locale_url_filter.rb
@@ -11,6 +11,18 @@ module Jekyll
       ).to_s
     end
 
+    def locale_url_test(path = '', locale = nil)
+      site_base_url = @context.registers[:site].baseurl
+      locale ||= @context.registers[:page]['lang']
+      collection = @context.registers[:page]['collection']
+      permalink = @context.registers[:site].collections[collection].metadata['permalink']
+
+      site_base_url.to_s + Jekyll::URL.new(
+        template: permalink,
+        placeholders: { collection: locale, path: path },
+      ).to_s
+    end
+
     def delocalize_url(path, locale = nil)
       locale ||= @context.registers[:page]['lang']
       path.gsub(%r{^/#{locale}/}, '/')
