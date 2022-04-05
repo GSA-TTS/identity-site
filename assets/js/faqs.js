5b162dc48854dc4695e5b5548e5e75be3cdf4ba6
ManishMShah <manishmshah@fcoh2j-k1b3hv2f.fios-router.home>
2022-04-05 11:58:25 -0400

    LG-5878-address-pr-comments-partner-redesign

---
+++ assets/js/faqs.js
@@ -0,0 +1,68 @@
+const openAccordionCount = {
+  general: 0,
+  logistics: 0,
+  development: 0,
+};
+
+function updateSingleAccordion(el, section) {
+  setTimeout(function () {
+    if (el.getAttribute('aria-expanded') == 'true') {
+      openAccordionCount[section]++;
+    } else {
+      openAccordionCount[section]--;
+    }
+    toggleExpandAllCollapseAllBtn(section); // needs help
+  }, 10);
+}
+
+function toggleExpandAllCollapseAllBtn(section) {
+  const btns = document.getElementsByClassName(`collapse-expand-btn-${section}`);
+
+  Array.from(btns).forEach((btn) => {
+    btn.innerHTML = openAccordionCount[section] > 0 ? 'Collapse all' : 'Expand all';
+  });
+}
+
+function updateAllAccordions(section) {
+  if (openAccordionCount[section] > 0) {
+    closeAllAccordions(section);
+  } else {
+    openAllAccordions(section);
+  }
+  toggleExpandAllCollapseAllBtn(section);
+}
+
+function closeAllAccordions(section) {
+  const accordionButtons = document.getElementsByClassName(`login-accordion-btn-${section}`);
+
+  Array.from(accordionButtons).forEach((el) => {
+    el.setAttribute('aria-expanded', 'false');
+  });
+
+  const accordionContentPanels = document.getElementsByClassName(
+    `login-accordion-content-${section}`,
+
+    Array.from(accordionContentPanels).forEach((el) => {
+      el.hidden = true;
+    }),
+  );
+
+  openAccordionCount[section] = 0;
+}
+
+function openAllAccordions(section) {
+  const accordionButtons = document.getElementsByClassName(`login-accordion-btn-${section}`);
+  openAccordionCount[section] = 0;
+
+  Array.from(accordionButtons).forEach((el) => {
+    el.setAttribute('aria-expanded', 'true');
+    openAccordionCount[section]++;
+  });
+
+  const accordionContentPanels = document.getElementsByClassName(
+    `login-accordion-content-${section}`,
+  );
+  Array.from(accordionContentPanels).forEach((el) => {
+    el.hidden = false;
+  });
+}
