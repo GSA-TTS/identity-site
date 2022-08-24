33053182c111a4f5af27e4840c0633551bbe232d
Mike Ogren <10132008+mogren-usds@users.noreply.github.com>
2022-08-24 10:15:27 -0400

    Add contact phone number (#928)
    Add phone number to Contact Us page, and tweak the pages organization to show the commonly asked questions first, followed by contact info.

---
+++ _plugins/escape_quotes.rb
@@ -0,0 +1,9 @@
+module Jekyll
+  module EscapeQuotes
+    def escape_quotes(input = '')
+      input.gsub(/'/){ "\\'" }
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::EscapeQuotes)
