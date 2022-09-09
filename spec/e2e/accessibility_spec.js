3d6c351018afcbddf75752e87058a4040252f073
Andrew Duthie <andrew.duthie@gsa.gov>
2022-09-09 11:48:05 -0400

    Move link exception handling to page querying

--- spec/e2e/accessibility_spec.js
+++ spec/e2e/accessibility_spec.js
@@ -1,7 +1,7 @@
 import { AxePuppeteer } from '@axe-core/puppeteer';
 import { toHaveNoViolations } from 'jest-axe';
 import { page, goto } from './support/browser';
-import { getLinks, toNotHaveTargetBlank } from './support/target-blank';
+import { getCandidateLinks, toNotHaveTargetBlank } from './support/target-blank';
 
 expect.extend(toHaveNoViolations);
 expect.extend({ toNotHaveTargetBlank });
@@ -13,29 +13,6 @@ const EXCLUDE_PATTERNS = [
   /\.pdf$/, // Puppeteer Chromium cannot preview PDF files
   /admin/,
 ];
-
-/**
- * Returns true if the element is inside a form, or false otherwise.
- *
- * @param {Element} element
- *
- * @return {boolean}
- */
-const isInForm = (element) => !!element.closest('form');
-
-/**
- * Returns true if the given link should be considered an exception to enforcement of links opening
- * in the same tab.
- *
- * Refer to WCAG guidance on qualifying situations.
- *
- * @see https://www.w3.org/TR/WCAG20-TECHS/G200.html
- *
- * @param {HTMLAnchorElement} link
- *
- * @return {boolean}
- */
-const isNewTabLinkException = (link) => isInForm(link);
 
 describe('accessibility', () => {
   const paths = JSON.parse(process.env.ALL_URLS)
@@ -74,10 +51,8 @@ describe('accessibility', () => {
           const results = await runner.analyze();
           expect(results).toHaveNoViolations();
 
-          const links = await getLinks(page);
-          links
-            .filter((link) => !isNewTabLinkException(link))
-            .forEach((a) => expect(a).toNotHaveTargetBlank());
+          const links = await getCandidateLinks(page);
+          links.forEach((a) => expect(a).toNotHaveTargetBlank());
         },
         TEST_TIMEOUT_MS,
       );
--- spec/e2e/support/target-blank.js
+++ spec/e2e/support/target-blank.js
@@ -3,19 +3,44 @@
  */
 
 /**
+ * Returns true if the element is inside a form, or false otherwise.
+ *
+ * @param {Element} element
+ *
+ * @return {boolean}
+ */
+const isInForm = (element) => !!element.closest('form');
+
+/**
+ * Returns true if the given link should be considered an exception to enforcement of links opening
+ * in the same tab.
+ *
+ * Refer to WCAG guidance on qualifying situations.
+ *
+ * @see https://www.w3.org/TR/WCAG20-TECHS/G200.html
+ *
+ * @param {HTMLAnchorElement} link
+ *
+ * @return {boolean}
+ */
+const isNewTabLinkException = (link) => isInForm(link);
+
+/**
  * @param {import('puppeteer').Page} page
  * @return {Promise<SimplifiedLink[]>}
  */
-function getLinks(page) {
+function getCandidateLinks(page) {
   return page.$$eval('a', (aTags) =>
-    aTags.map((a) =>
-      // Get the info we want across the Chrome DevTools Protocol
-      ({
-        innerText: a.innerText.trim(),
-        href: a.href,
-        target: a.target,
-      }),
-    ),
+    aTags
+      .filter((a) => !isNewTabLinkException(a))
+      .map((a) =>
+        // Get the info we want across the Chrome DevTools Protocol
+        ({
+          innerText: a.innerText.trim(),
+          href: a.href,
+          target: a.target,
+        }),
+      ),
   );
 }
 
@@ -29,4 +54,4 @@ function toNotHaveTargetBlank(a) {
   };
 }
 
-export { getLinks, toNotHaveTargetBlank };
+export { getCandidateLinks, toNotHaveTargetBlank };
