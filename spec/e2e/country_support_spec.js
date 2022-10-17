ca23efdf1c4a9b4f986bc7198809786a08039522
Zach Margolis <zachmargolis@users.noreply.github.com>
2022-10-17 08:39:39 -0700

    Update international country support table (#968)
    - "supports_X_unconfirmed" fields (new signups) overrides
      the plain "supports_X" fields so that the table more
      accurately reflects what new users experience
    
    - JSDoc for optional, not nullable
    
    Co-authored-by: Andrew Duthie <andrew.duthie@gsa.gov>

--- spec/e2e/country_support_spec.js
+++ spec/e2e/country_support_spec.js
@@ -16,6 +16,14 @@ describe('country support', () => {
         supports_sms: true,
         supports_voice: false,
       },
+      UY: {
+        name: 'Uruguay',
+        country_code: '598',
+        supports_sms: true,
+        supports_sms_unconfirmed: false,
+        supports_voice: true,
+        supports_voice_unconfirmed: false,
+      },
     },
   };
 
@@ -39,12 +47,22 @@ describe('country support', () => {
     await goto('/help/manage-your-account/international-phone-support/');
 
     const table = await page.waitForSelector('.js-country-support:not([hidden]) table');
-    expect((await table.$$('tbody tr')).length).toBe(2);
+    expect((await table.$$('tbody tr')).length).toBe(3);
     expect(await table.$eval('tbody tr:nth-child(1) td:nth-child(1)', (el) => el.innerText)).toBe(
       'Canada (CA)',
     );
     expect(await table.$eval('tbody tr:nth-child(2) td:nth-child(1)', (el) => el.innerText)).toBe(
       'United States (US)',
     );
+
+    expect(await table.$eval('tbody tr:nth-child(3) td:nth-child(1)', (el) => el.innerText)).toBe(
+      'Uruguay (UY)',
+    );
+    expect(
+      await table.$eval('tbody tr:nth-child(3) td:nth-child(3)', (el) => el.innerText.trim()),
+    ).toBe('No');
+    expect(
+      await table.$eval('tbody tr:nth-child(3) td:nth-child(4)', (el) => el.innerText.trim()),
+    ).toBe('No');
   });
 });
