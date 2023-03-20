3df7b6732587cbc8f99e8318d441c5cbec5ce3c7
Andrew Duthie <andrew.duthie@gsa.gov>
2023-03-20 16:22:44 -0400

    LG-9179: Sort Contact Us agencies case-insensitive
    Why: As a user submitting a support ticket, I expect that I can scan for the name of the relevant agency in an ordered list, so that I can quickly find and select it from the long list of options.

--- _plugins/sort_by_values.rb
+++ _plugins/sort_by_values.rb
@@ -4,7 +4,7 @@ module Jekyll
     # @return [Array<Hash>] entry-like hashes with 'key' and 'value'
     def sort_by_values(hash)
       hash.
-        sort_by { |_key, value| value }.
+        sort_by { |_key, value| value.downcase }.
         map { |key, value| { 'key' => key, 'value' => value } }
     end
   end
