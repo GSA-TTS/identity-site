e803cdc756b123a9be22529f4329117f7e23062b
Matt Gardner <wilburnforce@gmail.com>
2023-07-25 12:48:25 -0400

    Rename element

--- assets/js/post_office_search.js
+++ assets/js/post_office_search.js
@@ -2,5 +2,5 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AddressSearch from '@18f/identity-address-search';
 
-const root = createRoot(document.getElementById('search'));
+const root = createRoot(document.getElementById('post-office-search'));
 root.render(React.createElement(AddressSearch));
