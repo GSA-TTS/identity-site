92020ea8f5c0313fead8192c4dac463f1f137414
Matt Gardner <wilburnforce@gmail.com>
2023-08-07 13:39:37 -0400

    Configure address search URLs

--- assets/js/post_office_search.js
+++ assets/js/post_office_search.js
@@ -3,4 +3,9 @@ import { createRoot } from 'react-dom/client';
 import AddressSearch from '@18f/identity-address-search';
 
 const root = createRoot(document.getElementById('post-office-search'));
-root.render(React.createElement(AddressSearch));
+root.render(
+  React.createElement(AddressSearch, {
+    addressSearchURL: 'https://idp.dev.identitysandbox.gov/api/address_search',
+    locationsURL: 'https://idp.dev.identitysandbox.gov/api/usps_locations',
+  }),
+);
