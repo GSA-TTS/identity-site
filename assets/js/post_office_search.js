77f223e84103137c790e107f70745c07a87c24c4
Matt Gardner <wilburnforce@gmail.com>
2023-08-08 12:08:49 -0400

    Configure API endpoints through HTML data attributes

--- assets/js/post_office_search.js
+++ assets/js/post_office_search.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AddressSearch from '@18f/identity-address-search';
 
-const root = createRoot(document.getElementById('post-office-search'));
+const elem = document.getElementById('post-office-search');
+const root = createRoot(elem);
+const { addressSearchUrl, locationsSearchUrl } = elem.dataset;
+
 root.render(
   React.createElement(AddressSearch, {
-    addressSearchURL: 'https://idp.dev.identitysandbox.gov/api/address_search',
-    locationsURL: 'https://idp.dev.identitysandbox.gov/api/usps_locations',
+    addressSearchURL: addressSearchUrl,
+    locationsURL: locationsSearchUrl,
   }),
 );
