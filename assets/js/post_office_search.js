a71629f256423bd96821071c052fc784cdcff27b
Matt Gardner <wilburnforce@gmail.com>
2023-08-08 15:59:15 -0400

    LG-10303: Configure address search URLs (#1135)

--- assets/js/post_office_search.js
+++ assets/js/post_office_search.js
@@ -2,5 +2,13 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AddressSearch from '@18f/identity-address-search';
 
-const root = createRoot(document.getElementById('post-office-search'));
-root.render(React.createElement(AddressSearch));
+const elem = document.getElementById('post-office-search');
+const root = createRoot(elem);
+const { addressSearchUrl, locationsSearchUrl } = elem.dataset;
+
+root.render(
+  React.createElement(AddressSearch, {
+    addressSearchURL: addressSearchUrl,
+    locationsURL: locationsSearchUrl,
+  }),
+);
