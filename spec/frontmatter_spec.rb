c9826ed76a15ed185d797d011936030d81300566
Andrew Duthie <andrew.duthie@gsa.gov>
2023-08-22 08:50:03 -0400

    Add spec to check for duplicate redirects

--- spec/frontmatter_spec.rb
+++ spec/frontmatter_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 RSpec.describe '.md files' do
+  global_redirects = []
+
   Dir["#{REPO_ROOT}/content/**/*.md"].sort.each do |path|
     page = path.split(REPO_ROOT.to_s).last
 
@@ -9,6 +11,18 @@ RSpec.describe '.md files' do
         _, frontmatter, _content = File.read(path).split('---', 3)
 
         expect(YAMLHelper.duplicate_keys(frontmatter)).to be_empty
+      end
+
+      it 'includes globally unique redirects' do
+        _, frontmatter, _content = File.read(path).split('---', 3)
+        data = YAML.load(frontmatter)
+        redirects = Array(data['redirect_from'])
+
+        redirects.each do |redirect|
+          expect(global_redirects).not_to include(redirect), "Redirect already set:\n\n#{redirect}"
+        end
+
+        global_redirects += redirects
       end
     end
   end
