c19088e0dd3ca7a353f7e42ece19cfb4231493d9
Timothy Bradley <timothy.bradley@gsa.gov>
2023-08-28 19:02:07 -0700

    LG-9710: Add tests for PO search field input and error message

--- spec/js/post_office_search_spec.ts
+++ spec/js/post_office_search_spec.ts
@@ -2,19 +2,24 @@
  * @jest-environment jsdom
  */
 
-import { getByLabelText } from '@testing-library/dom';
+import { getByLabelText, getByRole, waitFor } from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import { act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { promisify } from 'util';
 
 const wait = promisify(setImmediate);
 
 describe('Post Office Search', () => {
   let container: HTMLDivElement;
+  let user: UserEvent;
   beforeEach(async () => {
+    user = userEvent.setup();
     await act(async () => {
       container = document.createElement('div');
       container.id = 'post-office-search';
+      container.dataset.addressSearchUrl = 'http://localhost:4000/api/address_search';
+      container.dataset.locationsSearchUrl = 'http://localhost:4000/api/usps_locations';
       document.body.appendChild(container);
       jest.isolateModules(() => {
         // eslint-disable-next-line global-require
@@ -39,6 +44,12 @@ describe('Post Office Search', () => {
     );
   };
 
+  const getButton = function () {
+    return getByRole(container, 'button', {
+      name: 'in_person_proofing.body.location.po_search.search_button',
+    });
+  };
+
   it('renders a post office search component', async () => {
     await act(async () => {
       const field = getField();
@@ -48,12 +59,35 @@ describe('Post Office Search', () => {
     });
   });
 
-  it('allows a text search', async () => {
-    await act(async () => {
+  describe('when searching', () => {
+    const testAddress = '1600 W Pennsylvania Ave, Washington, DC 20500';
+
+    it('allows search field input', async () => {
       const field = getField();
-      expect(field).toBeEnabled();
-      expect(field).toBeVisible();
-      await wait();
+      await act(async () => {
+        user.clear(field);
+        user.type(field, testAddress);
+        await wait();
+      });
+
+      await waitFor(
+        () => {
+          expect(field).toHaveValue(testAddress);
+        },
+        { timeout: 3000, interval: 50 },
+      );
+    });
+
+    describe('invalid input', () => {
+      it('shows an error', async () => {
+        const button = getButton();
+        const errorMessage = 'in_person_proofing.body.location.inline_error';
+        await act(async () => {
+          user.click(button);
+          await wait();
+        });
+        expect(container).toHaveTextContent(errorMessage);
+      });
     });
   });
 });
