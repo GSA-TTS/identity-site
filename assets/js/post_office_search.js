52c3713bd9a3b0377b9c35d4d37f37821e702b1f
gina-yamada <gina.yamada@gsa.gov>
2023-08-31 10:26:34 -0600

    pass react component into PostOfficeSearch as prop

--- assets/js/post_office_search.js
+++ assets/js/post_office_search.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AddressSearch from '@18f/identity-address-search';
+import PostOfficeNoResults from './post_office_no_results';
 
 const elem = document.getElementById('post-office-search');
 const root = createRoot(elem);
 const { addressSearchUrl, locationsSearchUrl } = elem.dataset;
 
 root.render(
-  React.createElement(AddressSearch, {
-    addressSearchURL: addressSearchUrl,
-    locationsURL: locationsSearchUrl,
-    onFoundLocations: () => {},
-  }),
+  <AddressSearch
+    addressSearchURL={addressSearchUrl}
+    locationsURL={locationsSearchUrl}
+    onFoundLocations={()=> {}}
+    PostOfficeNoResultsMessage={PostOfficeNoResults}
+  />
 );
