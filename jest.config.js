31723499d6e8d59865b0c85a3296cfef7573aeae
Timothy Bradley <timothy.bradley@gsa.gov>
2023-08-31 17:27:22 -0700

    LG-9710: Address identity-address-search import problem

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,10 @@ module.exports = {
     'node_modules/(?!(@18f/identity-address-search)/)',
     '\\.pnp\\.[^\\/]+$',
   ],
+  moduleNameMapper: {
+    '@18f/identity-address-search':
+      '<rootDir>/../node_modules/@18f/identity-address-search/dist/index.js',
+  },
   testMatch: ['**/*_spec.[jt]s'],
   globalSetup: '<rootDir>/e2e/support/setup.js',
   globalTeardown: '<rootDir>/e2e/support/teardown.js',
