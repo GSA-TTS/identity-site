4de5331978160a35c40b498f24c9117e53c10485
Andrew Duthie <andrew.duthie@gsa.gov>
2023-09-01 13:24:45 -0400

    Rename post_office_search to TSX

--- assets/js/post_office_search.tsx
+++ assets/js/post_office_search.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+// @ts-ignore
 import AddressSearch from '@18f/identity-address-search';
 
-const elem = document.getElementById('post-office-search');
+const elem = document.getElementById('post-office-search')!;
 const root = createRoot(elem);
 const { addressSearchUrl, locationsSearchUrl } = elem.dataset;
 
