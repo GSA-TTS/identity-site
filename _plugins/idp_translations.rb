f96785310271a236e0fad4f7eb4c42c280ecb292
Matt Gardner <wilburnforce@gmail.com>
2023-09-01 13:59:42 -0400

    Use block param to get config values

--- _plugins/idp_translations.rb
+++ _plugins/idp_translations.rb
@@ -9,24 +9,25 @@ require 'net/http'
 require 'json'
 require 'open-uri'
 
-DOMAIN = Jekyll.configuration({})['idp_base_url']
-LANGUAGES = Jekyll.configuration({})['languages']
-MANIFEST_FILE = "/packs/manifest.json"
-
-def download_files(origin_uris, destination)
+def download_files(origin_uris, destination, idp_domain)
   origin_uris.each do |uri|
     stripped_name = uri.split('.').last(2).join('.')
     FileUtils.mkdir_p(File.dirname("#{destination}/#{stripped_name}"))
-    download = URI.open("#{DOMAIN}#{uri}")
+    download = URI.open("#{idp_domain}#{uri}")
     IO.copy_stream(download, "#{destination}/#{stripped_name}")
   end
 end
 
 Jekyll::Hooks.register :site, :post_write do |site|
-  next if ([DOMAIN, LANGUAGES, MANIFEST_FILE].any?(&:nil?) | site.config['skip_idp_translations'])
+  idp_domain = site.config['idp_base_url']
+  languages = site.config['languages']
+  manifest_file =  site.config['idp_manifest_path']
+  skip_idp_translations = site.config['skip_idp_translations']
+
+  next if ([idp_domain, languages, manifest_file].any?(&:nil?) | skip_idp_translations)
   begin
     destination_translations_dir = File.join(site.config['destination'], 'assets/idp_translations')
-    origin_manifest_uri = URI("#{DOMAIN}#{MANIFEST_FILE}")
+    origin_manifest_uri = URI("#{idp_domain}#{manifest_file}")
     manifest_response_body = Net::HTTP.get_response(origin_manifest_uri).body
     manifest_hash = JSON.parse(manifest_response_body)
     document_capture_assets = manifest_hash.dig('entrypoints', 'document-capture', 'assets', 'js')
@@ -35,9 +36,9 @@ Jekyll::Hooks.register :site, :post_write do |site|
 
     # only download the translation assets
     document_capture_translations = document_capture_assets
-      .grep(/#{LANGUAGES.map {|str| "#{str}.js" }.join('|')}/)
+      .grep(/#{languages.map {|str| "#{str}.js" }.join('|')}/)
   
-    download_files(document_capture_translations, destination_translations_dir)
+    download_files(document_capture_translations, destination_translations_dir, idp_domain)
   rescue => exception
     puts exception
   end
