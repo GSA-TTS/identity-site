476593bdb4f6ca4c1198032fe29b91ee80771166
gina-yamada <gina.yamada@gsa.gov>
2023-09-13 11:25:16 -0600

    Use i18n package for translations

--- assets/js/no-in-person-locations-display.tsx
+++ assets/js/no-in-person-locations-display.tsx
@@ -1,9 +1,12 @@
+// @ts-ignore
+import { t } from '@18f/identity-i18n';
+
 interface NoInPersonLocationsDisplayProps {
   address: string;
 }
 
 function NoInPersonLocationsDisplay({ address }: NoInPersonLocationsDisplayProps) {
-  return <h2 role="status">in_person_proofing.body.location.po_search.none_found {address}</h2>;
+  return <h2 role="status">{t('in_person_proofing.body.location.po_search.none_found', { address })}</h2>;
 }
 
 export default NoInPersonLocationsDisplay;
