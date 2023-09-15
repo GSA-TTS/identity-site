a81e342a348d6a73fe04d42202b496f22091ca6c
gina-yamada <gina.yamada@gsa.gov>
2023-09-15 10:48:31 -0600

    fix linter errors

--- jest.config.js
+++ jest.config.js
@@ -7,8 +7,7 @@ module.exports = {
   moduleNameMapper: {
     '@18f/identity-address-search':
       '<rootDir>/../node_modules/@18f/identity-address-search/dist/index.js',
-    '@18f/identity-i18n':
-      '<rootDir>/../node_modules/@18f/identity-i18n/dist/index.js',
+    '@18f/identity-i18n': '<rootDir>/../node_modules/@18f/identity-i18n/dist/index.js',
   },
   testMatch: ['**/*_spec.[jt]s', '**/*_spec.[jt]s?'],
   globalSetup: '<rootDir>/e2e/support/setup.js',
