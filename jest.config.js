94dde59243c9b4d520156d751f00cae08f16d684
gina-yamada <gina.yamada@gsa.gov>
2023-09-15 15:02:40 -0600

    updated tesMatch

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,7 @@ module.exports = {
       '<rootDir>/../node_modules/@18f/identity-address-search/dist/index.js',
     '@18f/identity-i18n': '<rootDir>/../node_modules/@18f/identity-i18n/dist/index.js',
   },
-  testMatch: ['**/*_spec.[jt]s', '**/*_spec.[jt]s?'],
+  testMatch: ['**/*_spec.[jt]s?(x)'],
   globalSetup: '<rootDir>/e2e/support/setup.js',
   globalTeardown: '<rootDir>/e2e/support/teardown.js',
   testEnvironment: '<rootDir>/e2e/support/puppeteer-environment.js',
