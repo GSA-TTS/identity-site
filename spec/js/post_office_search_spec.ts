2235007bde01fb2608cb6231a19263ed856cd648
gina-yamada <gina.yamada@gsa.gov>
2023-10-05 12:08:40 -0600

    Update post office search tests

--- spec/js/post_office_search_spec.ts
+++ spec/js/post_office_search_spec.ts
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { getByLabelText, getByRole, waitFor } from '@testing-library/dom';
+import { getByLabelText, getByRole, waitFor, getAllByText } from '@testing-library/dom';
 import '@testing-library/jest-dom';
 import { act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -34,12 +34,12 @@ describe('Post Office Search', () => {
 
   let container: HTMLDivElement;
   let user: ReturnType<typeof userEvent['setup']>;
+
   beforeEach(async () => {
     user = userEvent.setup();
     await act(async () => {
       container = document.createElement('div');
       container.id = 'post-office-search';
-      container.dataset.addressSearchUrl = addressSearchUrl;
       container.dataset.locationsSearchUrl = locationsSearchUrl;
       document.body.appendChild(container);
       jest.isolateModules(() => {
@@ -60,52 +60,108 @@ describe('Post Office Search', () => {
     }
   });
 
-  const getField = function () {
+  const getAddressField = function () {
     return getByLabelText(
       document.body,
-      'in_person_proofing.body.location.po_search.address_search_label',
+      'in_person_proofing.body.location.po_search.address_label',
     );
   };
 
-  const getButton = function () {
+  const getCityField = function () {
+    return getByLabelText(
+      document.body,
+      'in_person_proofing.body.location.po_search.city_label',
+    );
+  };
+
+  const getStateField = function () {
+    return getByLabelText(
+      document.body,
+      'in_person_proofing.body.location.po_search.state_label',
+    );
+  };
+
+  const getZipcodeField = function () {
+    return getByLabelText(
+      document.body,
+      'in_person_proofing.body.location.po_search.zipcode_label',
+    );
+  };
+
+  const getSearchButton = function () {
     return getByRole(container, 'button', {
       name: 'in_person_proofing.body.location.po_search.search_button',
     });
   };
 
-  it('renders a post office search component', () => {
-    const field = getField();
-    expect(field).toBeEnabled();
-    expect(field).toBeVisible();
+  const testData = {
+    address:'1600 W Pennsylvania Ave',
+    city: 'Washington',
+    state: 'DC',
+    zipcode: '20500'
+  };
+
+  function getFormElements() {
+    const addressField = getAddressField();
+    const cityField = getCityField();
+    const stateField = getStateField();
+    const zipcodeField = getZipcodeField(); 
+    const searchButton = getSearchButton();
+    return { addressField, cityField, stateField, zipcodeField, searchButton }
+  }
+
+  it('renders post office search form fields that are enabled', () => {
+    const { addressField, cityField, stateField, zipcodeField, searchButton } = getFormElements();
+
+    expect(addressField).toBeEnabled();
+    expect(cityField).toBeEnabled();
+    expect(stateField).toBeEnabled();
+    expect(zipcodeField).toBeEnabled();
+
+    expect(addressField).toBeVisible();
+    expect(cityField).toBeVisible();
+    expect(stateField).toBeVisible();
+    expect(zipcodeField).toBeVisible();
+    expect(searchButton).toBeVisible();
   });
 
   describe('when searching', () => {
-    const testAddress = '1600 W Pennsylvania Ave, Washington, DC 20500';
-
     it('allows search field input', async () => {
-      const field = getField();
+      const { addressField, cityField, stateField, zipcodeField } = getFormElements();
+
       await act(async () => {
-        await user.clear(field);
-        await user.type(field, testAddress);
+        await user.clear(addressField);
+        await user.clear(cityField);
+        await user.clear(zipcodeField);
+
+        await user.type(addressField, testData.address);
+        await user.type(cityField, testData.city);
+        await user.selectOptions(stateField, testData.state);
+        await user.type(zipcodeField, testData.zipcode);
       });
 
       await waitFor(() => {
-        expect(field).toHaveValue(testAddress);
+        expect(addressField).toHaveValue(testData.address);
+        expect(cityField).toHaveValue(testData.city);
+        expect(stateField).toHaveValue(testData.state);
+        expect(zipcodeField).toHaveValue(testData.zipcode);
       });
     });
 
     describe('submitting', () => {
       describe('invalid input', () => {
         beforeEach(async () => {
-          const button = getButton();
+          const button = getSearchButton();
           await act(async () => {
             await user.click(button);
           });
         });
 
-        it('shows an error', () => {
-          const errorMessage = 'in_person_proofing.body.location.inline_error';
+        it('shows an error', async () => {
+          const errorMessage = 'simple_form.required.text';
+          const requiredErrorMessages = getAllByText(document.body, 'simple_form.required.text');
           expect(container).toHaveTextContent(errorMessage);
+          expect(requiredErrorMessages).toHaveLength(4);
         });
 
         it('does not result in any fetch requests', () => {
@@ -115,11 +171,19 @@ describe('Post Office Search', () => {
 
       describe('valid input with no results', () => {
         beforeEach(async () => {
-          const field = getField();
-          const button = getButton();
+          const { addressField, cityField, stateField, zipcodeField } = getFormElements();
+
+          const button = getSearchButton();
           await act(async () => {
-            await user.clear(field);
-            await user.type(field, testAddress);
+            await user.clear(addressField);
+            await user.clear(cityField);
+            await user.clear(zipcodeField);
+    
+            await user.type(addressField, testData.address);
+            await user.type(cityField, testData.city);
+            await user.selectOptions(stateField, testData.state);
+            await user.type(zipcodeField, testData.zipcode);
+
             await user.click(button);
           });
         });
