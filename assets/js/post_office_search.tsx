a47f36272f90e4659d36ef35262df8659bbf5716
gina-yamada <125507397+gina-yamada@users.noreply.github.com>
2023-10-17 11:33:39 -0600

    LG-11106 Address Search in Help Center should search on "enter" (#1175)
    * Wrapped FullAddressSearch in form
    
    * fix linter errors
    
    * added noValidate to form
    
    * remove trailing unnecessary comma

--- assets/js/post_office_search.tsx
+++ assets/js/post_office_search.tsx
@@ -13,25 +13,28 @@ const root = createRoot(elem);
 const { locationsSearchUrl } = elem.dataset;
 
 root.render(
-  <FullAddressSearch
-    disabled={false}
-    handleLocationSelect={null}
-    locationsURL={locationsSearchUrl}
-    noInPersonLocationsDisplay={NoInPersonLocationsDisplay}
-    onFoundLocations={() => {}}
-    registerField={() => {}}
-    resultsHeaderComponent={() => (
-      <Alert type="info" className="margin-bottom-4">
-        <strong>
-          {t('in_person_proofing.body.location.po_search.you_must_start.message', {
-            app_name: 'Login.gov',
-          })}
-        </strong>{' '}
-        <a href={t('in_person_proofing.body.location.po_search.you_must_start.link')}>
-          {t('in_person_proofing.body.location.po_search.you_must_start.link_text')}
-        </a>
-      </Alert>
-    )}
-    usStatesTerritories={UsStatesTerritories}
-  />,
+  <form>
+    <FullAddressSearch
+      noValidate
+      disabled={false}
+      handleLocationSelect={null}
+      locationsURL={locationsSearchUrl}
+      noInPersonLocationsDisplay={NoInPersonLocationsDisplay}
+      onFoundLocations={() => {}}
+      registerField={() => {}}
+      resultsHeaderComponent={() => (
+        <Alert type="info" className="margin-bottom-4">
+          <strong>
+            {t('in_person_proofing.body.location.po_search.you_must_start.message', {
+              app_name: 'Login.gov',
+            })}
+          </strong>{' '}
+          <a href={t('in_person_proofing.body.location.po_search.you_must_start.link')}>
+            {t('in_person_proofing.body.location.po_search.you_must_start.link_text')}
+          </a>
+        </Alert>
+      )}
+      usStatesTerritories={UsStatesTerritories}
+    />
+  </form>,
 );
