a4fbbf7c5f1dfe3f224e19ea7df0b2d024f9cbfa
gina-yamada <gina.yamada@gsa.gov>
2023-10-30 11:20:30 -0400

    removed disabled tests for po search

--- spec/e2e/post_office_search_spec.ts
+++ spec/e2e/post_office_search_spec.ts
@@ -11,67 +11,29 @@ describe('PO search page', () => {
     jest.setTimeout(10_000);
   });
 
-  describe('PO search page enabled', () => {
-    let it;
-    if (process.env.PO_SEARCH_TESTING_ENABLED) {
-      it = global.it;
-    } else {
-      it = global.it.skip;
-    }
+  it('is accessible from the side menu', async () => {
+    await goto('/help/verify-your-identity/overview/');
 
-    it('is accessible from the side menu', async () => {
-      await goto('/help/verify-your-identity/overview/');
+    const link = await page.waitForXPath(
+      '//a[contains(text(),"Find a Participating Post Office to finish identity verification")]',
+      { timeout: 1000 },
+    );
 
-      const link = await page.waitForXPath(
-        '//a[contains(text(),"Find a Participating Post Office to finish identity verification")]',
-        { timeout: 1000 },
-      );
+    expect(link).not.toBeUndefined();
 
-      expect(link).not.toBeUndefined();
+    await page.evaluate((a) => (a as HTMLAnchorElement).click(), link);
 
-      await page.evaluate((a) => (a as HTMLAnchorElement).click(), link);
+    await page.waitForNavigation();
 
-      await page.waitForNavigation();
-
-      expect(page!.url()).toContain(
-        '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
-      );
-    });
-
-    it('renders a PO search component', async () => {
-      await goto(
-        '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
-      );
-      await page.waitForSelector('#post-office-search input', { timeout: 1000 });
-    });
+    expect(page!.url()).toContain(
+      '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
+    );
   });
 
-  describe('PO search page disabled', () => {
-    let it;
-    if (process.env.PO_SEARCH_TESTING_ENABLED) {
-      it = global.it.skip;
-    } else {
-      it = global.it;
-    }
-
-    it('is not accessible from the side menu', async () => {
-      await goto('/help/verify-your-identity/overview/');
-
-      await expect(
-        page.waitForXPath(
-          '//a[contains(text(),"Find a Participating Post Office to finish identity verification")]',
-          { timeout: 1000 },
-        ),
-      ).rejects.toEqual(expect.anything());
-    });
-
-    it('does not render a PO search component', async () => {
-      await goto(
-        '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
-      );
-      await expect(
-        page.waitForSelector('#post-office-search input', { timeout: 1000 }),
-      ).rejects.toEqual(expect.anything());
-    });
+  it('renders a PO search component', async () => {
+    await goto(
+      '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
+    );
+    await page.waitForSelector('#post-office-search', { timeout: 1000 });
   });
 });
