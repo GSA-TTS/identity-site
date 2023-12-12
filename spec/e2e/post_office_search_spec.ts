0139d3cb5f9b428b22c64874e67a56cc16f5b329
Gina <125507397+gina-yamada@users.noreply.github.com>
2023-12-12 13:13:34 -0700

    LG-11267: Enable PO Search (#1183)
    * Enable PO Search, Display Side Nav
    
    * removed disabled tests for po search
    
    * remove unused scope block
    
    * removed path from defaults
    
    * Delete po_search_enabled config file
    
    * remove beforeEach block in test
    
    * clean up deleted yml file from circleci config
    
    * delete build-po-search code from circleci config
    
    * changelog: User-Facing Improvements, PO Search, enable po search
    
    * cleaned up build-po-search in config.yml
    
    * remove last instance of e2e-po-search
    
    * Add trailing period
    
    * Edit casing in title
    
    * Add link to grandchild page and modify content
    
    * fix linter error
    
    * revert linter fix
    
    * update case of post office for spanish translation

--- spec/e2e/post_office_search_spec.ts
+++ spec/e2e/post_office_search_spec.ts
@@ -7,71 +7,29 @@ const page = originalPage as Page;
 const goto = originalGoto as (path: string) => ReturnType<Page['goto']>;
 
 describe('PO search page', () => {
-  beforeEach(() => {
-    jest.setTimeout(10_000);
+  it('is accessible from the side menu', async () => {
+    await goto('/help/verify-your-identity/overview/');
+
+    const link = await page.waitForXPath(
+      '//a[contains(text(),"Find a Participating Post Office")]',
+      { timeout: 1000 },
+    );
+
+    expect(link).not.toBeUndefined();
+
+    await page.evaluate((a) => (a as HTMLAnchorElement).click(), link);
+
+    await page.waitForNavigation();
+
+    expect(page!.url()).toContain(
+      '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
+    );
   });
 
-  describe('PO search page enabled', () => {
-    let it;
-    if (process.env.PO_SEARCH_TESTING_ENABLED) {
-      it = global.it;
-    } else {
-      it = global.it.skip;
-    }
-
-    it('is accessible from the side menu', async () => {
-      await goto('/help/verify-your-identity/overview/');
-
-      const link = await page.waitForXPath(
-        '//a[contains(text(),"Find a Participating Post Office")]',
-        { timeout: 1000 },
-      );
-
-      expect(link).not.toBeUndefined();
-
-      await page.evaluate((a) => (a as HTMLAnchorElement).click(), link);
-
-      await page.waitForNavigation();
-
-      expect(page!.url()).toContain(
-        '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
-      );
-    });
-
-    it('renders a PO search component', async () => {
-      await goto(
-        '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
-      );
-      await page.waitForSelector('#post-office-search input', { timeout: 1000 });
-    });
-  });
-
-  describe('PO search page disabled', () => {
-    let it;
-    if (process.env.PO_SEARCH_TESTING_ENABLED) {
-      it = global.it.skip;
-    } else {
-      it = global.it;
-    }
-
-    it('is not accessible from the side menu', async () => {
-      await goto('/help/verify-your-identity/overview/');
-
-      await expect(
-        page.waitForXPath(
-          '//a[contains(text(),"Find a Participating Post Office to finish identity verification")]',
-          { timeout: 1000 },
-        ),
-      ).rejects.toEqual(expect.anything());
-    });
-
-    it('does not render a PO search component', async () => {
-      await goto(
-        '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
-      );
-      await expect(
-        page.waitForSelector('#post-office-search input', { timeout: 1000 }),
-      ).rejects.toEqual(expect.anything());
-    });
+  it('renders a PO search component', async () => {
+    await goto(
+      '/help/verify-your-identity/verify-your-identity-in-person/find-a-participating-post-office',
+    );
+    await page.waitForSelector('#post-office-search', { timeout: 1000 });
   });
 });
