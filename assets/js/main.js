76e9e1c10c370a39c83ee07dce8040e07b45f3da
Andrew Duthie <andrew.duthie@gsa.gov>
2024-03-07 14:49:59 -0500

    Update import for navigation component

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-import { accordion, banner, comboBox, datePicker, header } from '@18f/identity-design-system';
+import { accordion, banner, comboBox, datePicker, navigation } from '@18f/identity-design-system';
 
-const components = [accordion, banner, comboBox, datePicker, header];
+const components = [accordion, banner, comboBox, datePicker, navigation];
 components.forEach((component) => component.on());
