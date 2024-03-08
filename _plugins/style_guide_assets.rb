58bafbb8bdaf4b6b962463f219578395b665fe27
Andrew Duthie <andrew.duthie@gsa.gov>
2024-03-08 08:57:20 -0500

    Add initializer script to base layout

--- _plugins/style_guide_assets.rb
+++ _plugins/style_guide_assets.rb
@@ -5,28 +5,22 @@
 
 require 'fileutils'
 
-def copy_assets(origin, destination)
-  origin_glob = File.join(origin, '**/*')
-  origin_files = Dir.glob(origin_glob).reject { |f| File.directory?(f) }
+DESIGN_SYSTEM_ASSETS_TO_COPY = %w[
+  fonts
+  img
+  js/init.js
+]
 
-  origin_files.each do |origin_file|
-    filename = origin_file.gsub(origin, '').gsub(%r{^/}, '')
-    dest_filename = File.join(destination, filename)
-    next if File.exist?(dest_filename)
-
-    FileUtils.mkdir_p(File.dirname(dest_filename))
-    FileUtils.cp(origin_file, dest_filename)
-  end
-end
+DESIGN_SYSTEM_ASSETS_DIRECTORY = 'node_modules/@18f/identity-design-system/dist/assets'
 
 Jekyll::Hooks.register :site, :post_write do |site|
-  destination_fonts_dir = File.join(site.config['destination'], 'assets/fonts')
-  destination_img_dir = File.join(site.config['destination'], 'assets/img')
+  build_assets_directory = File.join(site.config['destination'], 'assets')
+  FileUtils.mkdir_p(build_assets_directory)
 
-  uswds_assets_root = File.join(Dir.pwd, 'node_modules/@18f/identity-design-system/dist/assets/')
-  origin_fonts_dir = File.join(uswds_assets_root, 'fonts')
-  origin_img_dir = File.join(uswds_assets_root, 'img')
-
-  copy_assets(origin_fonts_dir, destination_fonts_dir)
-  copy_assets(origin_img_dir, destination_img_dir)
+  DESIGN_SYSTEM_ASSETS_TO_COPY.each do |dir|
+    FileUtils.copy_entry(
+      File.join(DESIGN_SYSTEM_ASSETS_DIRECTORY, dir),
+      File.join(build_assets_directory, dir)
+    )
+  end
 end
