dda661f962301afc7d600c45363aa0ca26353348
Andrew Duthie <andrew.duthie@gsa.gov>
2024-03-28 08:45:36 -0400

    Compare all to first with clear aggregated error

--- spec/data_spec.rb
+++ spec/data_spec.rb
@@ -3,11 +3,16 @@ require 'spec_helper'
 
 RSpec.describe 'data' do
   it 'has the same keys across all locales' do
-    keys = Dir['_data/*/settings.yml'].map { |path| hash_keys(YAML.load_file(path)) }
+    keys = Dir['_data/*/settings.yml'].map { |path| [path, hash_keys(YAML.load_file(path))] }
 
-    expect(keys).not_to be_empty
-    keys.combination(2).each do |first, second|
-      expect(first).to match_array(second)
+    first_file_keys, *other_file_keys = keys
+    first_file, first_keys = first_file_keys
+    aggregate_failures do
+      other_file_keys.each do |other_file, other_keys|
+        expect(first_keys).to match_array(other_keys),
+                              "Expected `#{other_file}` to contain same keys as `#{first_file}`\n" \
+                              "Difference: #{first_keys - other_keys | other_keys - first_keys}"
+      end
     end
   end
 
