fa3bdfdf514b86152b6f9160a6c9b4ddbee087a2
Mitchell Henke <mitchell.henke@gsa.gov>
2024-04-26 09:54:12 -0500

    Check for static file rather than file or directory existing

--- spec/support/matchers.rb
+++ spec/support/matchers.rb
@@ -43,7 +43,7 @@ RSpec::Matchers.define :link_to_locale_pages do |locale|
       page = a[:href]
       link_path = URI::parse(page).path
       next if link_path.match(/partners/)
-      if !link_path.start_with?("/#{locale}/") && !File.exist?(File.join(REPO_ROOT, link_path))
+      if !link_path.start_with?("/#{locale}/") && !File.file?(File.join(REPO_ROOT, link_path))
         broken_links << a.to_html
       end
     end
