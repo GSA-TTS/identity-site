3a455ace76465f49ef4cf7b85d0577d899452ad4
Mitchell Henke <mitchell.henke@gsa.gov>
2024-04-26 10:55:34 -0500

    Update spec/all_pages_spec.rb
    Co-authored-by: Andrew Duthie <1779930+aduth@users.noreply.github.com>

--- spec/all_pages_spec.rb
+++ spec/all_pages_spec.rb
@@ -21,7 +21,6 @@ RSpec.describe 'all pages' do
   let(:old_paths) { YAML.load_file('OLD_URLS.yml').map { |url| URI(url).path } }
   let(:redirect_froms) do
     Dir["#{REPO_ROOT}/content/**/*.md"].map do |path|
-      page = path.split(REPO_ROOT.to_s).last
       frontmatter = File.read(path).split('---', 3)[1]
       Array(YAML.load(frontmatter)['redirect_from'])
     end.flatten
