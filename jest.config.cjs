f74f4ab3a7955509fe3ca81f7018b3b42582d2f8
Zach Margolis <zachary.margolis@gsa.gov>
2024-06-13 14:29:04 -0700

    Update some path regexes

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   rootDir: 'spec',
+  transform: {
+    "\\.(ts|tsx|js|jsx)": "ts-jest",
+  },
   transformIgnorePatterns: [
     'node_modules/(?!(@18f/identity-address-search|@18f/identity-i18n|@18f/identity-components)/)',
     '\\.pnp\\.[^\\/]+$'
@@ -11,7 +14,7 @@ module.exports = {
     '@18f/identity-i18n': '<rootDir>/../node_modules/@18f/identity-i18n/dist/index.js',
     '@18f/identity-components': '<rootDir>/../node_modules/@18f/identity-components/dist/index.js',
   },
-  testMatch: ['**/*_spec.[jt]s?(x)'],
+  testMatch: ['**/*_spec.(ts|tsx|js|jsx)'],
   testTimeout: 15000,
   globalSetup: '<rootDir>/e2e/support/setup.js',
   globalTeardown: '<rootDir>/e2e/support/teardown.js',
