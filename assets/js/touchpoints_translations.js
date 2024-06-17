6d42216537f99e17ccc808ef812b09972bbed3bf
Jack Cody <jack.cody@gsa.gov>
2024-06-17 11:20:04 -0500

    Add hook for onTouchpointsFormLoaded

--- assets/js/touchpoints_translations.js
+++ assets/js/touchpoints_translations.js
@@ -1,11 +1,17 @@
-const yesButton = document.querySelector('input[type=submit][value=yes]');
-const noButton = document.querySelector('input[type=submit][value=no]');
-const touchpointsRoot = document.getElementById('touchpoints_feedback');
-if (touchpointsRoot) {
+const initializeTouchpoints = () => {
+  const touchpointsRoot = document.getElementById('touchpoints_feedback');
+  if (!touchpointsRoot) return;
+
+  const yesButton = document.querySelector('input[type=submit][value=yes]');
+  const noButton = document.querySelector('input[type=submit][value=no]');
   const label = touchpointsRoot.querySelector('label');
+
   yesButton.value = touchpointsRoot.dataset.yes;
   noButton.value = touchpointsRoot.dataset.no;
+
   if (label) {
     label.innerText = touchpointsRoot.dataset.question;
   }
-}
+};
+
+document.addEventListener('onTouchpointsFormLoaded', initializeTouchpoints);
