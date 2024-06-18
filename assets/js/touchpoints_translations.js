13bf5befc849bf72f4f598867483870fdcbc9205
jc-gsa <104452882+jc-gsa@users.noreply.github.com>
2024-06-18 15:49:05 -0500

    LG-13650: Fix sporadic JavaScript bug related to Touchpoints (#1279)
    * Add hook for onTouchpointsFormLoaded
    
    * Refactor touchpoint translations
    
    * Store foreign touchpoints file locally

--- assets/js/touchpoints_translations.js
+++ assets/js/touchpoints_translations.js
@@ -1,11 +1,20 @@
-const yesButton = document.querySelector('input[type=submit][value=yes]');
-const noButton = document.querySelector('input[type=submit][value=no]');
-const touchpointsRoot = document.getElementById('touchpoints_feedback');
-if (touchpointsRoot) {
-  const label = touchpointsRoot.querySelector('label');
-  yesButton.value = touchpointsRoot.dataset.yes;
-  noButton.value = touchpointsRoot.dataset.no;
-  if (label) {
-    label.innerText = touchpointsRoot.dataset.question;
+const initializeTouchpointsTranslations = () => {
+  const touchpointsRoot = document.getElementById('touchpoints_feedback');
+
+  if (touchpointsRoot) {
+    const { yes: yesValue, no: noValue, question } = touchpointsRoot.dataset;
+
+    const yesButton = document.querySelector('input[type=submit][value=yes]');
+    const noButton = document.querySelector('input[type=submit][value=no]');
+    const label = touchpointsRoot.querySelector('label');
+
+    yesButton?.setAttribute('value', yesValue);
+    noButton?.setAttribute('value', noValue);
+
+    if (label) {
+      label.innerText = question;
+    }
   }
-}
+};
+
+document.addEventListener('onTouchpointsFormLoaded', initializeTouchpointsTranslations);
