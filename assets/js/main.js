feb0dcb10191a873715bb48677528193382905b4
Jack Cody <jack.cody@gsa.gov>
2024-06-25 18:45:22 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
 import { accordion, banner, comboBox, datePicker, navigation } from '@18f/identity-design-system';
+import { storeUrlQueryParams } from './query_params.js';
 
 const components = [accordion, banner, comboBox, datePicker, navigation];
 components.forEach((component) => component.on());
+
+storeUrlQueryParams();
