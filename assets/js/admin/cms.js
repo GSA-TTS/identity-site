8df1a79deed32686c1dee5f96c180325c0a3941b
Jack Cody <jack.cody@gsa.gov>
2025-01-08 00:25:43 -0600

    Change component CSS load

--- assets/js/admin/cms.js
+++ assets/js/admin/cms.js
@@ -2,3 +2,5 @@ import CMS from 'decap-cms';
 import ProgramUpdatePreview from './program_update_preview.js';
 
 CMS.registerPreviewTemplate('program_updates', ProgramUpdatePreview);
+
+window.CMS = CMS;
